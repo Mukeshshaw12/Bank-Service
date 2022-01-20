85f25a009f90bbb7a0cd7379717773f1997c7063
Mukeshshaw12 <mukeshshaw1012@gmail.com>
2022-01-20 21:53:59 +0530

    added database integration

--- src/main/java/com/example/demo/UserController.java
+++ src/main/java/com/example/demo/UserController.java
@@ -1,15 +1,19 @@
 package com.example.demo;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.entity.User;
+import com.example.demo.service.IUserService;
 
 @RestController
 public class UserController {
-	
+	@Autowired
+	IUserService userService;
 	@GetMapping("/user")
 	String getUser() {
 		System.out.println("testing");
@@ -20,5 +24,10 @@ public class UserController {
 	@PostMapping("/user")
 	void createUser(@RequestBody User user) {
 		System.out.println(user.getName());
+		userService.saveUser(user);
+	}
+	@DeleteMapping("/user")
+	void deleteUser() {
+		
 	}
 }
---
+++ src/main/java/com/example/demo/service/IUserService.java
@@ -0,0 +1,22 @@
+package com.example.demo.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.demo.UserRepository;
+import com.example.demo.entity.User;
+@Service
+
+public class IUserService {
+	
+	@Autowired //DependencyIngestionDI
+    UserRepository userRepository;
+	public void saveUser(User user) {
+		// TODO Auto-generated method stub
+		userRepository.save(user);
+		
+		
+		
+	}
+
+}
